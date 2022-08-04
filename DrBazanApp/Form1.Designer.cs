6072a9ecaeece72f9bd7370f87a398fc45689fb8
MateoLopez2000 <lm67427572@gmail.com>
2022-08-04 18:07:19 -0400

    first commit

---
+++ DrBazanApp/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace DrBazanApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxCameras = new System.Windows.Forms.ComboBox();
+            this.btnGrabar = new System.Windows.Forms.Button();
+            this.btnCapturar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxCameras
+            // 
+            this.cbxCameras.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxCameras.FormattingEnabled = true;
+            this.cbxCameras.ItemHeight = 37;
+            this.cbxCameras.Location = new System.Drawing.Point(76, 38);
+            this.cbxCameras.Name = "cbxCameras";
+            this.cbxCameras.Size = new System.Drawing.Size(610, 45);
+            this.cbxCameras.TabIndex = 0;
+            // 
+            // btnGrabar
+            // 
+            this.btnGrabar.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGrabar.Location = new System.Drawing.Point(705, 37);
+            this.btnGrabar.Name = "btnGrabar";
+            this.btnGrabar.Size = new System.Drawing.Size(171, 45);
+            this.btnGrabar.TabIndex = 1;
+            this.btnGrabar.Text = "Grabar";
+            this.btnGrabar.UseVisualStyleBackColor = true;
+            this.btnGrabar.Click += new System.EventHandler(this.btnGrabar_Click);
+            // 
+            // btnCapturar
+            // 
+            this.btnCapturar.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapturar.Location = new System.Drawing.Point(1621, 37);
+            this.btnCapturar.Name = "btnCapturar";
+            this.btnCapturar.Size = new System.Drawing.Size(171, 45);
+            this.btnCapturar.TabIndex = 2;
+            this.btnCapturar.Text = "Capturar";
+            this.btnCapturar.UseVisualStyleBackColor = true;
+            this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(76, 114);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 800);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(992, 114);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(800, 800);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1965, 992);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnCapturar);
+            this.Controls.Add(this.btnGrabar);
+            this.Controls.Add(this.cbxCameras);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxCameras;
+        private System.Windows.Forms.Button btnGrabar;
+        private System.Windows.Forms.Button btnCapturar;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ DrBazanApp/Form1.cs
@@ -0,0 +1,92 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrBazanApp
+{
+    public partial class Form1 : Form
+    {
+        private const string Format = "yyyyMMdd_hhmmss";
+        private string Path = @"C:\Users\Mlopez\Visual Studio\DrBazanApp\DrBazanApp\pictures\";
+        private bool HayDispositivos;
+        private FilterInfoCollection MisDispositivos;
+        private VideoCaptureDevice MiWebCam;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CargaDispositivos();
+        }
+
+        public void CargaDispositivos()
+        {
+            MisDispositivos = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if(MisDispositivos.Count > 0)
+            {
+                HayDispositivos = true;
+                for(int i = 0; i < MisDispositivos.Count; i++)
+                {
+                    cbxCameras.Items.Add(MisDispositivos[i].Name.ToString());
+                    cbxCameras.Text = MisDispositivos[0].Name.ToString();
+                }
+            }
+            else
+            {
+                HayDispositivos = false;
+            }
+        }
+
+        private void CerrarWebCam()
+        {
+            if (MiWebCam != null && MiWebCam.IsRunning)
+            {
+                MiWebCam.SignalToStop();
+                MiWebCam = null;
+            }
+        }
+
+        private void btnGrabar_Click(object sender, EventArgs e)
+        {
+            CerrarWebCam();
+            int i = cbxCameras.SelectedIndex;
+            string NombreVideo = MisDispositivos[i].MonikerString;
+            MiWebCam = new VideoCaptureDevice(NombreVideo);
+            MiWebCam.NewFrame += new NewFrameEventHandler(Capturando);
+            MiWebCam.Start();
+        }
+
+        private void Capturando(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap Imagen = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = Imagen;
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            CerrarWebCam();
+        }
+
+        private void btnCapturar_Click(object sender, EventArgs e)
+        {
+            if (MiWebCam != null && MiWebCam.IsRunning)
+            {
+                pictureBox2.Image = pictureBox1.Image;
+
+                pictureBox2.Image.Save(Path + "Reporte Dr. Bazan" + DateTime.UtcNow.ToString("yyyy-MM-dd HH-mm-ss") + ".jpg", ImageFormat.Jpeg);
+            }
+        }
+    }
+}
